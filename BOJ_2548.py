c8ff491fdea65c4d83d87313223ce983365ea158
seochanhyeok <mushroom1324@naver.com>
2022-12-20 09:23:49 +0900

    math

---
+++ BOJ_2548.py
@@ -0,0 +1,16 @@
+import sys
+
+N = int(sys.stdin.readline())
+
+arr = list(map(int, sys.stdin.readline().split()))
+
+sums = []
+for i in arr:
+    sum = 0
+    for j in arr:
+        sum += abs(i - j)
+
+    sums.append((sum, i))
+
+sums.sort()
+print(sums[0][1])
