995d2878c83a6b6d605cd711b1c900971bba135e
seochanhyeok <mushroom1324@naver.com>
2023-03-04 23:17:20 +0900

    dpdpdpdpdpdp

---
+++ dp/BOJ_9655.py
@@ -0,0 +1,20 @@
+import sys
+# SK = 1, CY = 0
+dp = [-1] * 1001
+
+dp[1] = 1
+dp[2] = 0
+dp[3] = 1
+
+N = int(sys.stdin.readline())
+
+for i in range(4, N + 1):
+    if dp[i - 1] == 1:
+        dp[i] = 0
+    else:
+        dp[i] = 1
+
+if dp[N] == 1:
+    print("SK")
+else:
+    print("CY")
