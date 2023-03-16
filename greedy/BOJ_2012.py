d54e081f3e23bd6ee6a9cbf65cdaf50ff6ce053a
seochanhyeok <mushroom1324@naver.com>
2023-03-16 21:05:24 +0900

    umm

--- greedy/BOJ_2012.py
+++ greedy/BOJ_2012.py
@@ -1,9 +1,17 @@
 N=int(input())
-a=[0]*N
-for i in range(N):
-    a[i]=int(input())
-a.sort()
+L=list(int(input())for _ in range(N))
+L.sort()
 d=0
 for i in range(N):
-    d+=abs(a[i]-i-1)
+    d+=abs(L[i]-i-1)
 print(d)
+
+"""
+what the shit is this .. ? 
+
+_,*l=map(int,open(0))
+print(sum(abs(x-i-1)for i,x in enumerate(sorted(l))))
+
+"""
+
+
