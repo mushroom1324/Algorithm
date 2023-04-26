a00d03873ed11cc39d4b4c8eb77eb1f0ad7811e4
seochanhyeok <mushroom1324@naver.com>
2023-04-26 17:05:23 +0900

    tree traversal with bfs

---
+++ graph-theory/LEET_230. Kth Smallest Element in a BST.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+
+        a = set()
+        if not root:    return -1
+        deq = collections.deque([(root)])
+        while deq:
+            cur = deq.popleft()
+            a.add(cur.val)
+            if cur.left:   deq.append((cur.left))
+            if cur.right:  deq.append((cur.right))
+        a = sorted(a)
+        return a[k - 1]
+
