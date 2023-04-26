eb4203a7a700de9224bccdf192ecf7f3c211a524
seochanhyeok <mushroom1324@naver.com>
2023-04-26 17:17:06 +0900

    bfs

--- graph-theory/LEET_230. Kth Smallest Element in a BST.py
+++ graph-theory/LEET_230. Kth Smallest Element in a BST.py
@@ -7,6 +7,36 @@
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
 
+        stack = []
+        cur = root
+
+        while True:
+            while cur:
+                stack.append(cur)
+                cur = cur.left
+
+            if not stack:
+                break
+
+            node = stack.pop()
+            k -= 1
+
+            if k == 0:
+                return node.val
+
+            cur = node.right
+
+
+"""
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+
         a = set()
         if not root:    return -1
         deq = collections.deque([(root)])
@@ -17,4 +47,4 @@ class Solution:
             if cur.right:  deq.append((cur.right))
         a = sorted(a)
         return a[k - 1]
-
+"""
