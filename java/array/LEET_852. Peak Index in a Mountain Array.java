24904e9eaa716403485f49dc062535ec2d8b676f
Seo Chanhyeok <mushroom1324@naver.com>
2023-07-25 21:52:23 +0900

    Create LEET_852. Peak Index in a Mountain Array.java

---
+++ java/array/LEET_852. Peak Index in a Mountain Array.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int peakIndexInMountainArray(int[] arr) {
+        int i = 0;
+        int j = arr.length - 1;
+        int n = j + 1;
+
+        while(i <= j) {
+            int mid = (i + j) / 2;
+
+            if ((mid == 0 || arr[mid - 1] < arr[mid]) && (mid == n - 1 || arr[mid + 1] < arr[mid])) return mid;
+            else if (mid > 0 && arr[mid - 1] > arr[mid]) j = mid - 1;
+            else i = mid + 1;
+        }
+        return -1;
+    }
+}
