0e937a97175eb99b3efb334dc17225f2c6ca39cd
Seo Chanhyeok <mushroom1324@naver.com>
2023-09-08 15:31:18 +0900

    Update LEET_143. Reorder List.java

--- java/linked-list/LEET_143. Reorder List.java
+++ java/linked-list/LEET_143. Reorder List.java
@@ -41,3 +41,57 @@ class Solution {
         
     }
 }
+
+
+
+
+// solution using fast and slow pointer... but still time complexity is not so fast
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+
+    private int len;
+    private Stack<ListNode> stack = new Stack<>();
+
+    public void reorderList(ListNode head) {
+
+        // put all elements from the half of the node to the stack.
+        ListNode slow = head;
+        ListNode fast = head;
+        
+        while ( fast != null && fast.next != null ) {
+            slow = slow.next;
+            fast = fast.next.next;
+            ++len;
+        }
+
+        while (slow != null) {
+            stack.add(slow);
+            slow = slow.next;
+        }
+
+        // insert the top node of the stack accordingly.
+        // repeat it (len) times.
+        slow = head;
+
+        for(int i = 0; i < len ; ++i) {
+            ListNode temp = slow.next; 
+            ListNode endNode = stack.pop(); 
+            slow.next = endNode; 
+            endNode.next = temp; 
+            slow = temp; 
+        }
+        
+        // delete cycle of the last node.
+        slow.next = null;
+        
+    }
+}
